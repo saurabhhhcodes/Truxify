f10c3d4f3f7fc13d5d8def4da58afff93a9f2494
KanishJebaMathewM <kanishjebamathew.m@gmail.com>
2026-05-16 12:40:46 +0530

    updated workflow

--- apps/customer/lib/main.dart
+++ apps/customer/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  // Trigger a customer app scan.
+  
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const FreightFairApp());
 }
--- apps/driver/lib/main.dart
+++ apps/driver/lib/main.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
-  // Trigger a driver app scan.
+  
   runApp(const TruxifyApp());
 }
